fa6173dc2f2dd38e5cb5b73e9b81ae7e39b03fec
Amit Timalsina <amittimalsina14@gmail.com>
2020-07-07 06:20:08 +0545

    Adding basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { FooterComponent } from './footer/footer.component';
+import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ArticlesComponent,
+    FooterComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
